7bad71d2ab585956b963dac0c4521d11f6ef74ce
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2021-11-27 19:15:04 +0700

    Add intrinsics
    Signed-off-by: Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>

--- app/src/main/java/site/arksana/latihancompose/layout/Intrinsics.kt
+++ app/src/main/java/site/arksana/latihancompose/layout/Intrinsics.kt
@@ -1,4 +1,50 @@
 package site.arksana.latihancompose.layout
 
-class Intrinsics {
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import site.arksana.latihancompose.ui.theme.LatihanComposeTheme
+
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(
+            color = Color.Black, modifier = Modifier
+                .fillMaxHeight()
+                .width(1.dp)
+        )
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    LatihanComposeTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
 }
